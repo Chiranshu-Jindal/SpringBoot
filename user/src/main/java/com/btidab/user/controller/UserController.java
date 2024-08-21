0d640c3bc00ffe82f7804edeaa1afabc7f69d82c
chiranshu-jindal <chiranshujindal03@gmail.com>
2024-08-22 00:20:23 +0530

    Merge branch 'main' of https://github.com/Chiranshu-Jindal/SpringBoot.git into main

--- user/src/main/java/com/btidab/user/controller/UserController.java
+++ user/src/main/java/com/btidab/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.btidab.user.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,36 +20,41 @@ import com.btidab.user.service.UserService;
 
 @RestController
 public class UserController {
-	List<User> users = new ArrayList();
+	List<User> users = new ArrayList<>();
 	@Autowired
 	private UserService service;
-	
 
-	@GetMapping
-	public List<UserModel> getAllUser(){
+	@GetMapping("/users")
+	public List<UserModel> getAllUser() {
 		return service.getalluser();
 	}
-	
-//	@GetMapping("/{id}")
-//	public ResponseEntity<UserModel> getById(@PathVariable long id){
-//		UserModel u= service.getbyid(id)
-//				.orElseThrow(null)
-//		
-//		return service.getbyid();
-//	}
-	
+
+	@GetMapping("/users/{id}")
+	public ResponseEntity<UserModel> getById(@PathVariable long id) {
+		Optional<UserModel> u = service.getbyid(id);
+		if (u.isPresent()) {
+			return ResponseEntity.ok(u.get());
+		} else {
+			return null;
+		}
+	}
+
 	@PostMapping("/users")
-		public UserModel insertdata(@RequestBody UserModel user) {
-			return service.createuser(user);
-			}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteuser(@PathVariable Long id){
+	public UserModel insertdata(@RequestBody UserModel user) {
+		return service.createuser(user);
+	}
+
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Void> deleteuser(@PathVariable Long id) {
 		service.deleteuser(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	
-	
-	
+
+	@PutMapping("/users/{id}")
+	public ResponseEntity<UserModel> updateuser(@PathVariable Long id, @RequestBody UserModel data) {
+		UserModel updateduser = service.updateuser(id, data);
+		return ResponseEntity.ok(updateduser);
 	}
+
+}
+//return service.getbyid();
--- user/src/main/java/com/btidab/user/service/UserService.java
+++ user/src/main/java/com/btidab/user/service/UserService.java
@@ -15,21 +15,49 @@ public class UserService {
 	@Autowired
 	private UserRepositery repo;
 
-	//Create
+	// Create
 	public UserModel createuser(UserModel u) {
 		return repo.save(u);
 	}
-	//GetAll
+
+	// GetAll
 	public List<UserModel> getalluser() {
 		return repo.findAll();
 	}
-	//GetById
-	public Optional<UserModel> getbyid(long id){
+
+	// GetById
+	public Optional<UserModel> getbyid(long id) {
 		return repo.findById(id);
 	}
-	//Delete
+
+	// Delete
 	public void deleteuser(Long id) {
 		repo.deleteById(id);
 	}
-	
+
+	// update
+	public UserModel updateuser(Long id, UserModel data) {
+
+		UserModel a = repo.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+		a.setUsername(data.getUsername());
+		a.setPassword(data.getPassword());
+		a.setFirst_name(data.getFirst_name());
+		a.setLast_name(data.getLast_name());
+		a.setEmail(data.getEmail());
+		a.setTelephone(data.getTelephone());
+		return repo.save(a);
+	}
+
 }
+
+//Optional<UserModel> a = repo.findById(id);if(a.isPresent())
+//{
+//	a.setUsername(data.getUsername());
+//	a.setPassword(data.getPassword());
+//	a.setFirst_name(data.getFirst_name());
+//	a.setLast_name(data.getLast_name());
+//	a.setEmail(data.getEmail());
+//	a.setTelephone(data.getTelephone());
+//	return repo.save(a);
+//	
+//}
